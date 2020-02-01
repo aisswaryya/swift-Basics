339c6eb20bcfd7386a8f52f1482bc8712a1fcc93
aisswaryya <murugananth.a@husky.neu.edu>
2020-02-01 10:33:11 -0500

    Swift variable

---
+++ Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+import Foundation
+
+var r = 39
+var Pi = 3.14
+var radius = Pi * Double (r * r)
+print("Area of Circle is \(radius)")
+var a : String = "Northeastern"
+var b = "Smartphone"
+print(a + b)
+var num1: Int  = 12
+var power = 2
+//var result:Double = Double(power (Double(num1),Double(power)))
+//func result = pow(_num1:Int,_power:Decimal) -> Decimal
+let result = pow(Double(num1),Double(power))
+print(result)
+var 😘 = "iphone"
+var 😃 = "iphone\u{301}"
+var compare = 😘 == 😃
+print (compare)
+
+
+
